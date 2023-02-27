137cf7e028218ccfa0d37ca8ea7b0e59d8a19203
waterfog9580 <waterfog9580@gmail.com>
2023-02-27 15:40:51 +0900

    :tada: 프로젝트 구성

---
+++ src/main/kotlin/com/waterfogsw/timedeal/Application.kt
@@ -0,0 +1,11 @@
+package com.waterfogsw.timedeal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeDealApplication
+
+fun main(args: Array<String>) {
+  runApplication<TimeDealApplication>(*args)
+}
