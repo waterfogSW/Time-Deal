744583ac04ac1ed374e0ce46e2c1fc0794436f7a
waterfog9580 <waterfog9580@gmail.com>
2023-02-28 11:16:41 +0900

    :sparkles: JpaConfig 작성

---
+++ src/main/kotlin/com/waterfogsw/timedeal/common/config/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.waterfogsw.timedeal.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
