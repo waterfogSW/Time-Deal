48c2f202cb0bf848bbd3b13a4306524c71aeae70
waterfog9580 <waterfog9580@gmail.com>
2023-03-01 23:31:05 +0900

    :sparkles: redis 세션 스토리지 사용 설정

--- src/main/kotlin/com/waterfogsw/timedeal/common/config/RedisConfig.kt
+++ src/main/kotlin/com/waterfogsw/timedeal/common/config/RedisConfig.kt
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 class RedisConfig(
     private val redisProperties: RedisProperties,
 ) {
