bb7f6f6cdf8054b43f3ab3e54ab76aa2e7ac7fed
waterfog9580 <waterfog9580@gmail.com>
2023-03-02 13:06:09 +0900

    :sparkles: RedisTemplate 구현

--- src/main/kotlin/com/waterfogsw/timedeal/common/config/RedisConfig.kt
+++ src/main/kotlin/com/waterfogsw/timedeal/common/config/RedisConfig.kt
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @Configuration
@@ -12,6 +15,15 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 class RedisConfig(
     private val redisProperties: RedisProperties,
 ) {
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
+        redisTemplate.setConnectionFactory(connectionFactory)
+        return redisTemplate
+    }
 
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
