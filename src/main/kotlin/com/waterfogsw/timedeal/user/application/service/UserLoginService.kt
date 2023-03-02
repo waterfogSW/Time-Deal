432fd0fd3903ef9a7cced3259b373f964c08733b
waterfog9580 <waterfog9580@gmail.com>
2023-03-02 13:27:47 +0900

    :sparkles: 로그인 기능 서비스 구현

--- src/main/kotlin/com/waterfogsw/timedeal/user/application/service/UserLoginService.kt
+++ src/main/kotlin/com/waterfogsw/timedeal/user/application/service/UserLoginService.kt
@@ -2,14 +2,18 @@ package com.waterfogsw.timedeal.user.application.service
 
 import com.waterfogsw.timedeal.user.adapter.`in`.web.dto.UserLoginRequest
 import com.waterfogsw.timedeal.user.application.port.`in`.UserLoginCommand
-import com.waterfogsw.timedeal.user.domain.User
+import com.waterfogsw.timedeal.user.application.port.out.UserLookupPort
 import com.waterfogsw.timedeal.user.domain.UserSession
 import org.springframework.stereotype.Service
 
 @Service
-class UserLoginService : UserLoginCommand {
+class UserLoginService(
+    private val userLookupPort: UserLookupPort,
+) : UserLoginCommand {
 
     override fun login(userLoginRequest: UserLoginRequest): UserSession {
-        return UserSession("0x00", "test", User.Role.USER)
+        val user = userLookupPort.findByUsername(userLoginRequest.username)
+        user.checkPassword(userLoginRequest.password)
+        return UserSession.from(user)
     }
 }
--- src/main/kotlin/com/waterfogsw/timedeal/user/domain/User.kt
+++ src/main/kotlin/com/waterfogsw/timedeal/user/domain/User.kt
@@ -1,11 +1,20 @@
 package com.waterfogsw.timedeal.user.domain
 
+import com.waterfogsw.timedeal.common.exception.LoginFailedException
+
 data class User(
     val id: String? = null,
     val username: String,
     val password: String,
     val role: Role = Role.USER,
 ) {
+
+    fun checkPassword(inputPassword: String) {
+        if (password != inputPassword) {
+            throw LoginFailedException("Password incorrect")
+        }
+    }
+
     enum class Role {
         ADMIN, USER
     }
