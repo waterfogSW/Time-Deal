99d70821553efde73c1e04f2eb54975a9971db5d
waterfog9580 <waterfog9580@gmail.com>
2023-03-02 14:18:05 +0900

    :sparkles: 회원 로그아웃 API 컨트롤러 구현

---
+++ src/main/kotlin/com/waterfogsw/timedeal/user/adapter/in/web/UserLogoutController.kt
@@ -0,0 +1,21 @@
+package com.waterfogsw.timedeal.user.adapter.`in`.web
+
+import jakarta.servlet.http.HttpSession
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/users/logout")
+class UserLogoutController(
+    private val httpSession: HttpSession
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun logout() {
+        httpSession.invalidate()
+    }
+}
