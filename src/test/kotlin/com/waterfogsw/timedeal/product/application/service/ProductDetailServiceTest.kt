b2f36f9909e7fb69d3e549436adf32fd53a66ea5
waterfog9580 <waterfog9580@gmail.com>
2023-03-03 21:17:37 +0900

    :sparkles: 상품 상세 조회 API 서비스 테스트 구현

---
+++ src/test/kotlin/com/waterfogsw/timedeal/product/application/service/ProductDetailServiceTest.kt
@@ -0,0 +1,56 @@
+package com.waterfogsw.timedeal.product.application.service
+
+import com.waterfogsw.timedeal.product.adapter.`in`.web.dto.ProductDetailResponse
+import com.waterfogsw.timedeal.product.application.port.`in`.ProductLookupPort
+import com.waterfogsw.timedeal.product.application.service.mapper.ProductDTOMapper
+import com.waterfogsw.timedeal.product.domain.Product
+import io.kotest.core.annotation.DisplayName
+import io.kotest.core.spec.style.DescribeSpec
+import io.mockk.*
+import org.assertj.core.api.Assertions.assertThat
+import java.time.LocalDateTime
+
+@DisplayName("Service - 상품 상세 조회 기능")
+class ProductDetailServiceTest : DescribeSpec({
+
+    val productLookupPort = mockk<ProductLookupPort>()
+    val productDTOMapper = mockk<ProductDTOMapper>()
+    val productDetailService = ProductDetailService(productLookupPort, productDTOMapper)
+
+    describe("detail(id)") {
+        val productId = 1L
+        val product = Product(
+            id = productId,
+            name = "iPhone 13",
+            description = "A new iPhone model from Apple",
+            imageUrl = "https://example.com/image.jpg",
+            quantity = 10,
+            originalPrice = 1000000,
+            sellingPrice = 1200000,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+        )
+        val productDetailResponse = ProductDetailResponse(
+            id = productId,
+            name = "iPhone 13",
+            description = "A new iPhone model from Apple",
+            imageUrl = "https://example.com/image.jpg",
+            quantity = 10,
+            originalPrice = 1000000,
+            sellingPrice = 1200000,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+        )
+
+        every { productLookupPort.findById(productId) } returns product
+        every { productDTOMapper.mapToDetailResponse(product) } returns productDetailResponse
+
+        it("해당 id 값의 상품 상세를 반환한다") {
+            val result = productDetailService.detail(productId)
+
+            assertThat(productDetailResponse).isEqualTo(result)
+            verify { productLookupPort.findById(productId) }
+            verify { productDTOMapper.mapToDetailResponse(product) }
+        }
+    }
+})
