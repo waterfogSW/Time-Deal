8b75c549787ee9483b3a42cbf5f2fc95ea28f853
waterfog9580 <waterfog9580@gmail.com>
2023-03-03 16:11:02 +0900

    :sparkles: 상품 등록 권한 확인 로직 추가

--- src/main/kotlin/com/waterfogsw/timedeal/product/adapter/in/web/ProductCreateController.kt
+++ src/main/kotlin/com/waterfogsw/timedeal/product/adapter/in/web/ProductCreateController.kt
@@ -1,7 +1,9 @@
 package com.waterfogsw.timedeal.product.adapter.`in`.web
 
+import com.waterfogsw.timedeal.common.annotation.Auth
 import com.waterfogsw.timedeal.product.adapter.`in`.web.dto.ProductCreateRequest
 import com.waterfogsw.timedeal.product.application.port.`in`.ProductCreateCommand
+import com.waterfogsw.timedeal.user.domain.User.Role.*
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -11,6 +13,7 @@ class ProductCreateController(
     private val productCreateCommand: ProductCreateCommand,
 ) {
 
+    @Auth([ADMIN])
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody productCreateRequest: ProductCreateRequest) {
--- src/test/kotlin/com/waterfogsw/timedeal/product/adapter/in/web/ProductCreateControllerTest.kt
+++ src/test/kotlin/com/waterfogsw/timedeal/product/adapter/in/web/ProductCreateControllerTest.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.ninjasquad.springmockk.MockkBean
 import com.waterfogsw.timedeal.product.adapter.`in`.web.dto.ProductCreateRequest
 import com.waterfogsw.timedeal.product.application.port.`in`.ProductCreateCommand
+import com.waterfogsw.timedeal.user.domain.User
+import com.waterfogsw.timedeal.user.domain.UserPrincipal
 import com.waterfogsw.timedeal.util.restDocMockMvcBuild
 import io.kotest.core.annotation.DisplayName
 import io.kotest.core.spec.style.DescribeSpec
@@ -11,6 +13,7 @@ import io.mockk.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
+import org.springframework.mock.web.MockHttpSession
 import org.springframework.restdocs.ManualRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post
@@ -35,40 +38,75 @@ class ProductCreateControllerTest(
 
     describe("POST /api/products") {
 
-        val productCreateRequest = ProductCreateRequest(
-            name = "iPhone 13",
-            description = "A new iPhone model from Apple",
-            imageUrl = "https://example.com/image.jpg",
-            quantity = 10,
-            originalPrice = 1000000,
-            sellingPrice = 1200000,
-        )
+        context("관리자가 요청하면") {
 
-        every { productCreateCommand.create(productCreateRequest) } just runs
+            val user = UserPrincipal("0x00", "test", User.Role.ADMIN)
+            val mockHttpSession = MockHttpSession()
+            mockHttpSession.setAttribute(UserPrincipal.SESSION_NAME, user)
 
-        it("상품을 등록한다") {
-            mockMvc
-                .perform(
-                    post("/api/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(productCreateRequest)),
-                )
-                .andExpect(status().isCreated)
-                .andDo(
-                    document(
-                        "create-product",
-                        requestFields(
-                            fieldWithPath("name").description("The name of the product"),
-                            fieldWithPath("description").description("The description of the product"),
-                            fieldWithPath("imageUrl").description("The URL of the image of the product"),
-                            fieldWithPath("quantity").description("The quantity of the product"),
-                            fieldWithPath("originalPrice").description("The original price of the product"),
-                            fieldWithPath("sellingPrice").description("The selling price of the product"),
+            val productCreateRequest = ProductCreateRequest(
+                name = "iPhone 13",
+                description = "A new iPhone model from Apple",
+                imageUrl = "https://example.com/image.jpg",
+                quantity = 10,
+                originalPrice = 1000000,
+                sellingPrice = 1200000,
+            )
+
+            every { productCreateCommand.create(productCreateRequest) } just runs
+
+            it("상품을 등록하고 201 응답을 반환한다") {
+                mockMvc
+                    .perform(
+                        post("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(productCreateRequest))
+                            .session(mockHttpSession),
+                    )
+                    .andExpect(status().isCreated)
+                    .andDo(
+                        document(
+                            "create-product",
+                            requestFields(
+                                fieldWithPath("name").description("The name of the product"),
+                                fieldWithPath("description").description("The description of the product"),
+                                fieldWithPath("imageUrl").description("The URL of the image of the product"),
+                                fieldWithPath("quantity").description("The quantity of the product"),
+                                fieldWithPath("originalPrice").description("The original price of the product"),
+                                fieldWithPath("sellingPrice").description("The selling price of the product"),
+                            ),
                         ),
-                    ),
-                )
+                    )
 
-            verify { productCreateCommand.create(productCreateRequest) }
+                verify { productCreateCommand.create(productCreateRequest) }
+            }
+        }
+
+        context("사용자가 요청하면") {
+
+            val user = UserPrincipal("0x00", "test", User.Role.USER)
+            val mockHttpSession = MockHttpSession()
+            mockHttpSession.setAttribute(UserPrincipal.SESSION_NAME, user)
+
+            val productCreateRequest = ProductCreateRequest(
+                name = "iPhone 13",
+                description = "A new iPhone model from Apple",
+                imageUrl = "https://example.com/image.jpg",
+                quantity = 10,
+                originalPrice = 1000000,
+                sellingPrice = 1200000,
+            )
+
+            it("401 응답을 반환한다") {
+                mockMvc
+                    .perform(
+                        post("/api/products")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(productCreateRequest))
+                            .session(mockHttpSession),
+                    )
+                    .andExpect(status().isUnauthorized)
+            }
         }
     }
 })
